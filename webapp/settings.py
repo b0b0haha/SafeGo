e6845466eac1144067ffabd1507ab1726d159481
yan <wliyan9705@gmail.com>
2020-07-05 18:33:20 +0800

    前端页面添加了样式

--- webapp/settings.py
+++ webapp/settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'runoob',
-        'HOST':'127.0.0.1',
-        'PORT':3306,
-        'USER':'root',
-        'PASSWORD':'12345',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
