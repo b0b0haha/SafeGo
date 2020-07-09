94d9e3c28d5cffca716e43d315546254428c17d2
2bebetter <2bebetter@348296974@qq.com>
2020-07-09 13:00:55 +0800

    添加路由

--- django/safego/routes/urls.py
+++ django/safego/routes/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from routes import views, search
 
 urlpatterns = [
-    path('riskIndex/', views.risk_index().post),
-    re_path(r"^search_advise/$", search.search_advise, name="search_advise"),
-    re_path(r"^search_risk/$", search.search_risk, name="search_risk"),
+    re_path(r"search_advise/$", search.search_advise, name="search_advise"),
+    re_path(r"search_risk/$", search.search_risk, name="search_risk"),
+    re_path(r"search_simple/$", search.search_simple, name="search_risk"),
+    re_path(r"search_detail/$", search.search_detail, name="search_risk"),
 ]
